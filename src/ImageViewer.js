f24f5a2f9c0edaf53d127e06a303e9ed13e64a9d
David Harvey <dharvey0310@gmail.com>
2016-10-28 12:30:22 +0100

    0.1.3

---
+++ src/ImageViewer.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react'
+import {IoChevronRight, IoChevronLeft, IoCloseRound} from 'react-icons/lib/io'
+
+export default class ImageViewer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.loadPreviousImage = this.loadPreviousImage.bind(this)
+        this.loadNextImage = this.loadNextImage.bind(this)
+
+        this.length = this.props.images.length - 1
+        this.state = {currentIndex: this.props.index}
+    }
+
+    loadPreviousImage() {
+        this.setState({currentIndex: this.state.currentIndex - 1})
+    }
+
+    loadNextImage() {
+        this.setState({currentIndex: this.state.currentIndex + 1})
+    }
+
+    getContainerStyles() {
+        return {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: this.props.inverted ? `rgba(255,255,255, ${this.props.opacity ? this.props.opacity : 0.8})` : this.props.clear ? `rgba(0, 0, 0, 0)` : `rgba(0, 0, 0, ${this.props.opacity ? this.props.opacity : 0.8})`,
+            position: "fixed",
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0",
+            zIndex: "9998"
+        }
+    }
+
+    getArrowStyles() {
+        if (this.props.arrowStyles) {
+            return this.props.arrowStyles
+        } else {
+            return {
+                fontSize: "5em",
+                color: this.props.inverted ? "#000000" : "#ffffff",
+                cursor: "pointer"
+            }
+        }
+    }
+
+    getCloseStyles() {
+        if (this.props.closeStyles) {
+            return this.props.closeStyles
+        } else {
+            return {
+                position: "fixed",
+                top: "1px",
+                right: "1px",
+                zIndex: "9999",
+                fontSize: "5em",
+                color: this.props.inverted ? "#000000" : "#ffffff",
+                cursor: "pointer"
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            <div style={this.getContainerStyles()} className={`${this.props.containerClass ? this.props.containerClass : ""}`}>
+                {this.state.currentIndex !== 0 && !this.props.hideArrows ?
+                    <div>
+                        <IoChevronLeft onClick={() => this.loadPreviousImage()} style={this.getArrowStyles()} />
+                    </div> : null}
+                <div style={{maxWidth: "70%"}}>
+                    <img src={this.props.images[this.state.currentIndex]} className={`${this.props.imageClass ? this.props.imageClass : ""}`} style={this.props.imageStyles ? this.props.imageStyles : {}} />
+                </div>
+                {this.state.currentIndex !== this.length && !this.props.hideArrows ?
+                    <div>
+                        <IoChevronRight onClick={() => this.loadNextImage()} style={this.getArrowStyles()} />
+                    </div> : null}
+            </div>
+            <div>
+                <IoCloseRound style={this.getCloseStyles()} onClick={() => this.props.handleClose()} />
+            </div>
+            </div>
+        )
+    }
+}
